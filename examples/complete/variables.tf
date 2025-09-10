731c6bb976fc20636c084c7af3a19262dd2cc462
Guido Montano <gmontano@gocloud.la>
2025-09-10 15:44:18 -0300

    feat: add examples folder with updated documentation

---
+++ examples/complete/variables.tf
@@ -0,0 +1,27 @@
+/*----------------------------------------------------------------------*/
+/* Common |                                                             */
+/*----------------------------------------------------------------------*/
+
+# variable "metadata" {
+#   type = any
+# }
+
+# variable "project" {
+#   type = string
+# }
+
+/*----------------------------------------------------------------------*/
+/* EC2 | Variable Definition                                            */
+/*----------------------------------------------------------------------*/
+
+variable "ec2_instance_defaults" {
+  description = "Map of default values which will be used for each ec2 database."
+  type        = any
+  default     = {}
+}
+
+variable "ec2_instance_parameters" {
+  description = "Maps of ec2 databases to create a wrapper from. Values are passed through to the module."
+  type        = any
+  default     = {}
+}
