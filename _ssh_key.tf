ca0cb2e992bd379ec61ec226cc0f529ab07d5bc4
Guido Montano <gmontano@gocloud.la>
2025-11-06 17:30:23 -0300

    fix: update local ssh_key_parameters variable

--- _ssh_key.tf
+++ _ssh_key.tf
@@ -1,20 +1,17 @@
 locals {
-  ssh_key_parameters_tmp = [
+  ssh_key_parameters = {
     for ec2_instance_key, ec2_instance_config in var.ec2_instance_parameters :
-    {
-      "${ec2_instance_key}" = {
-        create                = try(ec2_instance_config.create_ssh_key, true)
-        create_private_key    = try(ec2_instance_config.create_private_key, false)
-        key_name              = try(ec2_instance_config.key_name, "${local.common_name}-${ec2_instance_key}")
-        key_name_prefix       = try(ec2_instance_config.key_name_prefix, null)
-        private_key_algorithm = try(ec2_instance_config.private_key_algorithm, "RSA")
-        private_key_rsa_bits  = try(ec2_instance_config.private_key_rsa_bits, 4096)
-        public_key            = sensitive(try(ec2_instance_config.public_key, ""))
-        tags                  = merge(lookup(ec2_instance_config, "tags", local.default_common_tags), { Name = "${local.common_name}-${ec2_instance_key}-key" })
-      }
+    ec2_instance_key => {
+      create                = try(ec2_instance_config.create_key, true)
+      create_private_key    = try(ec2_instance_config.create_private_key, false)
+      key_name              = try(ec2_instance_config.key_name, "${local.common_name}-${ec2_instance_key}")
+      key_name_prefix       = try(ec2_instance_config.key_name_prefix, null)
+      private_key_algorithm = try(ec2_instance_config.private_key_algorithm, "RSA")
+      private_key_rsa_bits  = try(ec2_instance_config.private_key_rsa_bits, 4096)
+      public_key            = sensitive(try(ec2_instance_config.public_key, ""))
+      tags                  = merge(lookup(ec2_instance_config, "tags", local.default_common_tags), { Name = "${local.common_name}-${ec2_instance_key}-key" })
     } if try(ec2_instance_config.create_key, false)
-  ]
-  ssh_key_parameters = merge(flatten(local.ssh_key_parameters_tmp)...)
+  }
 }
 
 module "key-pair" {
