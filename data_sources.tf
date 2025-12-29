8feb559f81d735f74a4f45489366bfec0b5ad4f9
Ignacio Bisio <ibisio@gocloud.la>
2025-12-29 16:45:25 -0300

    fix(data): change name in security_group_name

--- data_sources.tf
+++ data_sources.tf
@@ -37,7 +37,7 @@ data "aws_security_group" "default" {
   vpc_id = data.aws_vpc.this[each.key].id
 
   tags = {
-    Name = try(each.value.aws_security_group_name, local.default_security_group)
+    Name = try(each.value.security_group_name, local.default_security_group)
   }
 }
 
